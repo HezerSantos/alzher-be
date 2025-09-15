8c64ff15edd371cf45ad16e696efc7068daccdc5
HezerSantos <HezerSantos24@gmail.com>
2025-09-15 03:04:57 -0500

    Progress on FastAPI api

--- src/controllers/dashboard/POST/postDashboardDocument.ts
+++ src/controllers/dashboard/POST/postDashboardDocument.ts
@@ -1,8 +1,24 @@
+import axios from "axios";
+import FormData from "form-data";
 import { RequestHandler } from "express";
 
 const postDashboardDocument: RequestHandler = async(req, res, next) => {
     try{
-        const files = req.files
+        const files = req.files as Express.Multer.File[]
+
+        const formData = new FormData()
+
+        files?.forEach((file) => {
+            formData.append('files', file.buffer, file.originalname)
+        })
+        const response = await axios.post("http://localhost:5000/dashboard/scan/predict",
+            formData,
+            {
+                headers: formData.getHeaders()
+            }
+        )
+        
+        console.log(response)
         console.log(files)
 
         res.end()
