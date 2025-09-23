0dd5b2e44670e39f31b1fca29a9f97cb380bc694
HezerSantos <HezerSantos24@gmail.com>
2025-09-23 15:01:23 -0500

    Created the analytics info header data fetch

--- src/controllers/dashboard/GET/getDashboardAnalytics.ts
+++ src/controllers/dashboard/GET/getDashboardAnalytics.ts
@@ -23,8 +23,8 @@ const getDashboardAnalytics: RequestHandler = async(req, res, next) => {
             _count: { category: true }, 
             orderBy: { _count: { category: 'desc' } }, 
             take: 1
-        });
-
+        }) ?? []
+        
         //Returns the category that is most spent
         const largestCategoryExpense = await prisma.transaction.groupBy({
             by: ['category'],
@@ -74,8 +74,75 @@ const getDashboardAnalytics: RequestHandler = async(req, res, next) => {
             return acc
         }, 0) / monthlyExpense.length
 
-        console.log(totalSpent)
-        console.log(averageMonthlyExpense)
+        
+        //Returns a spendingPerDay[] ordered by desc
+
+        const spendingsPerDay = await prisma.transaction.groupBy({
+            by:['day'],
+            where: { userId: userId },
+            _sum: {
+                amount: true
+            },
+            orderBy: {_sum: { amount: 'desc' } }
+        })  
+
+        //Highest Day and Lowest Day
+        const highestDay = spendingsPerDay[0].day
+        const lowestDay = spendingsPerDay[spendingsPerDay.length - 1].day
+
+        const totalTransactionCount = await prisma.transaction.aggregate({
+            where: {
+                userId: userId
+            },
+           _count: true
+        })
+
+
+    interface DashboardAnalyticsInfoType {
+            header: string,
+            amountSpent: number | null,
+            primarySubHeader: string | null,
+            primarySubValue: string | null,
+            secondarySubHeader: string | null,
+            secondarySubValue: string | null
+        }
+
+        const dashboardAnalyticsInfo: DashboardAnalyticsInfoType[] = [
+            {
+                header: "Total Spent",
+                amountSpent: totalSpent._sum.amount,
+                primarySubHeader: "Frequent Category",
+                primarySubValue: mostFrequentCategory[0].category,
+                secondarySubHeader: "Largest Expense",
+                secondarySubValue: largestCategoryExpense[0].category
+            },
+            {
+                header: "Yearly Average",
+                amountSpent: yearlyAverage,
+                primarySubHeader: "Peak Month",
+                primarySubValue: largestMonthlyExpense[0].month,
+                secondarySubHeader: null,
+                secondarySubValue: null
+            },
+            {
+                header: "Monthly Average",
+                amountSpent: averageMonthlyExpense,
+                primarySubHeader: "Highest Day",
+                primarySubValue: `${highestDay} of the month`,
+                secondarySubHeader: "Lowest Day",
+                secondarySubValue: `${lowestDay} of the month`
+            },
+            {
+                header: "Total Transactions",
+                amountSpent: totalTransactionCount._count,
+                primarySubHeader: null,
+                primarySubValue: null,
+                secondarySubHeader: null,
+                secondarySubValue: null
+            }
+        ]
+
+        console.log(dashboardAnalyticsInfo)
 
         res.end()
     } catch(error){
