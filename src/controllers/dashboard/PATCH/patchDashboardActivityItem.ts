e04d23a9967d512717a7edd8960135156334eac6
HezerSantos <HezerSantos24@gmail.com>
2025-10-03 01:02:29 -0500

    Added validation for patch activity api

--- src/controllers/dashboard/PATCH/patchDashboardActivityItem.ts
+++ src/controllers/dashboard/PATCH/patchDashboardActivityItem.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from "express";
 import prisma from "../../../config/prisma";
 import throwError from "../../../helpers/errorHelper";
+import validatePatchDashboardActivityItem from "../../../validation/dashboard/validatePatchDashboardActivityItem";
+import { validationResult } from "express-validator";
 
 const reversedMonthMap: Map<number, string> = new Map([
   [1, "Jan"],
@@ -18,45 +20,54 @@ const reversedMonthMap: Map<number, string> = new Map([
 ]);
 
 
-const patchDashboardActivityItem: RequestHandler = async(req, res, next) => {
-    try{
-        const userId = req.user.id
-        const transactionId = req.params.id
-        const updatedTransactionBody = req.body
-        const transaction = await prisma.transaction.findUnique({
-            where: { id: transactionId }
-        })
+const patchDashboardActivityItem: RequestHandler[] = [
+    ...validatePatchDashboardActivityItem,
+    async(req, res, next) => {
+        try{
+            const errros = validationResult(req)
+            if(!errros.isEmpty()){
+                throwError("Invalid Body", 400, { msg: "Invalid Fields", code: "INVALID_BODY", validationErrors: errros.array()})
+            }
 
-        if(!transaction){
-            throwError("Transaction Not Found", 404, {msg: "Transaction Not Found", code: "INVALID_TRANSACTION_REFERENCE"})
-        }
-        if(transaction?.userId !== userId){
-            throwError("Unauthorized Transaction Permissions", 403, {msg: "Unauthorized Transaction Permissions", code: "INVALID_TRANSACTION_PERMISSIONS"})
-        }
 
-        const [ month, day, year ] = updatedTransactionBody.transactionDate.split("/")
+            const userId = req.user.id
+            const transactionId = req.params.id
+            const updatedTransactionBody = req.body
+            const transaction = await prisma.transaction.findUnique({
+                where: { id: transactionId }
+            })
 
-        const updatedTransactionData = {
-            category: transaction?.category !== updatedTransactionBody.category? updatedTransactionBody.category : undefined,
-            description: transaction?.description !== updatedTransactionBody.description? updatedTransactionBody.description : undefined,
-            amount: Number(transaction?.amount ) !== Number(updatedTransactionBody.transactionAmount)? Number(updatedTransactionBody.transactionAmount) : undefined,
-            day: Number(transaction?.day ) !== Number(day)? Number(day) : undefined,
-            month: transaction?.month !== reversedMonthMap.get(month)? reversedMonthMap.get(month): undefined,
-            year: Number(transaction?.year ) !== Number(year)? Number(year) : undefined
-        }
+            if(!transaction){
+                throwError("Transaction Not Found", 404, {msg: "Transaction Not Found", code: "INVALID_TRANSACTION_REFERENCE"})
+            }
+            if(transaction?.userId !== userId){
+                throwError("Unauthorized Transaction Permissions", 403, {msg: "Unauthorized Transaction Permissions", code: "INVALID_TRANSACTION_PERMISSIONS"})
+            }
 
-        const filteredTransactionData = Object.fromEntries(Object.entries(updatedTransactionData).filter(([key, value]) => value !== undefined))
-        if(Object.keys(filteredTransactionData).length === 0){
-            return res.end()
+            const [ month, day, year ] = updatedTransactionBody.transactionDate.split("/")
+
+            const updatedTransactionData = {
+                category: transaction?.category !== updatedTransactionBody.category? updatedTransactionBody.category : undefined,
+                description: transaction?.description !== updatedTransactionBody.description? updatedTransactionBody.description : undefined,
+                amount: Number(transaction?.amount ) !== Number(updatedTransactionBody.transactionAmount)? Number(updatedTransactionBody.transactionAmount) : undefined,
+                day: Number(transaction?.day ) !== Number(day)? Number(day) : undefined,
+                month: transaction?.month !== reversedMonthMap.get(month)? reversedMonthMap.get(month): undefined,
+                year: Number(transaction?.year ) !== Number(year)? Number(year) : undefined
+            }
+
+            const filteredTransactionData = Object.fromEntries(Object.entries(updatedTransactionData).filter(([key, value]) => value))
+            if(Object.keys(filteredTransactionData).length === 0){
+                return res.end()
+            }
+
+            await prisma.transaction.update({
+                where: { id: transactionId},
+                data: filteredTransactionData
+            })
+            res.end()
+        } catch (error) {
+            next(error)
         }
-        await prisma.transaction.update({
-            where: { id: transactionId},
-            data: filteredTransactionData
-        })
-        res.end()
-    } catch (error) {
-        next(error)
     }
-}
-
+]
 export default patchDashboardActivityItem
---
+++ src/validation/dashboard/validatePatchDashboardActivityItem.ts
@@ -0,0 +1,41 @@
+import { body } from 'express-validator'
+
+
+const categorySet = new Set([
+    "Leisure",
+    "Subscriptions",
+    "Dining",
+    "Grocery",
+    "Transportation"
+])
+
+const validatePatchDashboardActivityItem = [
+     body("category")
+        .trim()
+        .notEmpty()
+        .custom((value) => {
+            if(!categorySet.has(value)){
+                throw new Error("Invalid Category")
+            }
+            return true
+        }),
+    body("description")
+        .trim()
+        .notEmpty().withMessage("Description is required"),
+    body("transactionDate")
+        .trim()
+        .notEmpty().withMessage("Transaction date is required")
+        .custom((value) => {
+            const regex = /^([1-9]|1[0-2])\/([1-9]|[12][0-9]|3[01])\/\d{4}$/;
+            if (!regex.test(value)) {
+                throw new Error("Transaction date must be in M/D/YYYY format");
+            }
+            return true;
+        }),
+    body("transactionAmount")
+        .trim()
+        .notEmpty()
+        .isNumeric().withMessage("Invalid Amount")
+]
+
+export default validatePatchDashboardActivityItem
